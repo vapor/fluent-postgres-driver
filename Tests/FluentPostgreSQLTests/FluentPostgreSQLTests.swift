26dc631a5d31781bf7c067c5e49856a99a795229
Tanner Nelson <me@tanner.xyz>
2018-01-19 21:38:06 -0500

    tests passing

--- Tests/FluentPostgreSQLTests/FluentPostgreSQLTests.swift
+++ Tests/FluentPostgreSQLTests/FluentPostgreSQLTests.swift
@@ -42,20 +42,19 @@ class FluentPostgreSQLTests: XCTestCase {
         try benchmarker.benchmarkAutoincrement_withSchema()
     }
 
-//    func testNestedStruct() throws {
-//        database.logger = DatabaseLogger { print($0) }
-//        let conn = try database.makeConnection(using: .init(), on: eventLoop).await(on: eventLoop)
-//        try! User.prepare(on: conn).await(on: eventLoop)
-//
-//        let user = try! User(id: nil, name: "Tanner", pet: Pet(name: "Zizek"))
-//            .save(on: conn).await(on: eventLoop)
-//
-//        let fetched = try! User.query(on: conn).first().await(on: eventLoop)
-//        print(fetched?.pet)
-//
-//        try User.revert(on: conn).await(on: eventLoop)
-//        conn.close()
-//    }
+    func testNestedStruct() throws {
+        let conn = try database.makeConnection(using: .init(), on: eventLoop).await(on: eventLoop)
+        try User.prepare(on: conn).await(on: eventLoop)
+
+        let user = try User(id: nil, name: "Tanner", pet: Pet(name: "Zizek"))
+            .save(on: conn).await(on: eventLoop)
+
+        let fetched = try User.query(on: conn).first().await(on: eventLoop)
+
+        XCTAssertEqual(user.id, fetched?.id)
+        try User.revert(on: conn).await(on: eventLoop)
+        conn.close()
+    }
 
     static let allTests = [
         ("testSchema", testSchema),
@@ -67,33 +66,21 @@ class FluentPostgreSQLTests: XCTestCase {
         ("testAutoincrement", testAutoincrement),
     ]
 }
-//
-//struct Pet: Codable, PostgreSQLColumnStaticRepresentable, PostgreSQLDataCustomConvertible {
-//    static let postgreSQLColumn = PostgreSQLColumn(type: .jsonb)
-//    var name: String
-//
-//    func convertToPostgreSQLData() throws -> PostgreSQLData {
-//        return try .data(JSONEncoder().encode(self))
-//    }
-//
-//    static func convertFromPostgreSQLData(from data: PostgreSQLData) throws -> Pet {
-//        switch data {
-//        case .data(let data): return try JSONDecoder().decode(Pet.self, from: data)
-//        default: fatalError()
-//        }
-//    }
-//}
-//
-//final class User: PostgreSQLModel, Migration {
-//    static let idKey = \User.id
-//    var id: Int?
-//    var name: String
-//    var pet: Pet
-//
-//    init(id: Int? = nil, name: String, pet: Pet) {
-//        self.id = id
-//        self.name = name
-//        self.pet = pet
-//    }
-//}
+
+struct Pet: PostgreSQLJSONType {
+    var name: String
+}
+
+final class User: PostgreSQLModel, Migration {
+    static let idKey = \User.id
+    var id: Int?
+    var name: String
+    var pet: Pet
+
+    init(id: Int? = nil, name: String, pet: Pet) {
+        self.id = id
+        self.name = name
+        self.pet = pet
+    }
+}
 
