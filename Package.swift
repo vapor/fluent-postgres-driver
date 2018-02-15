b190df8e5c3d29ba94f17b42426330b09404943b
Tanner Nelson <me@tanner.xyz>
2018-02-14 21:51:22 -0500

    prerelease version updates

--- Package.swift
+++ Package.swift
@@ -9,16 +9,16 @@ let package = Package(
     ],
     dependencies: [
         // Swift Promises, Futures, and Streams.
-        .package(url: "https://github.com/vapor/async.git", .exact("1.0.0-beta.1")),
+        .package(url: "https://github.com/vapor/async.git", "1.0.0-beta.1"..<"1.0.0-beta.2"),
 
         // Core extensions, type-aliases, and functions that facilitate common tasks.
-        .package(url: "https://github.com/vapor/core.git", .exact("3.0.0-beta.1")),
+        .package(url: "https://github.com/vapor/core.git", "3.0.0-beta.1"..<"3.0.0-beta.2"),
 
         // Swift ORM framework (queries, models, and relations) for building NoSQL and SQL database integrations.
-        .package(url: "https://github.com/vapor/fluent.git", .exact("3.0.0-beta.2")),
+        .package(url: "https://github.com/vapor/fluent.git", "3.0.0-beta.2"..<"3.0.0-beta.3"),
 
         // Pure Swift, async/non-blocking client for PostgreSQL.
-        .package(url: "https://github.com/vapor/postgresql.git", .exact("1.0.0-beta.2")),
+        .package(url: "https://github.com/vapor/postgresql.git", "1.0.0-beta.2"..<"1.0.0-beta.3"),
     ],
     targets: [
         .target(name: "FluentPostgreSQL", dependencies: ["Async", "CodableKit", "Fluent", "FluentSQL", "PostgreSQL"]),
