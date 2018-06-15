47985c047df1770f9ac95b26d152bc187653373a
tanner0101 <me@tanner.xyz>
2018-06-14 22:55:54 -0400

    move postgresql string model

--- Sources/FluentPostgreSQL/PostgreSQLModel.swift
+++ Sources/FluentPostgreSQL/PostgreSQLModel.swift
@@ -8,17 +8,6 @@ extension PostgreSQLModel {
     public static var idKey: IDKey { return \.id }
 }
 
-public protocol PostgreSQLStringModel: Model where Self.Database == PostgreSQLDatabase, Self.ID == String {
-    /// This model's unique identifier.
-    var id: String? { get set }
-}
-
-extension PostgreSQLStringModel {
-    /// See `Model`.
-    public static var idKey: IDKey { return \.id }
-}
-
-
 public protocol PostgreSQLPivot: Pivot, PostgreSQLModel { }
 
 public protocol PostgreSQLMigration: Migration where Self.Database == PostgreSQLDatabase { }
