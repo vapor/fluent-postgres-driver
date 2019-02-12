6662a2b5307c183b05db8cb2fc8531285b96ccc2
Tanner Nelson <me@tanner.xyz>
2019-02-11 19:12:30 -0500

    move to master branch

--- Package.swift
+++ Package.swift
@@ -4,12 +4,11 @@ import PackageDescription
 let package = Package(
     name: "fluent-postgres-driver",
     products: [
-        // Swift ORM for PostgreSQL (built on top of Fluent ORM framework)
         .library(name: "FluentPostgresDriver", targets: ["FluentPostgresDriver"]),
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/fluent-kit.git", .branch("master")),
-        .package(url: "https://github.com/vapor/postgresql.git", .branch("2")),
+        .package(url: "https://github.com/vapor/postgresql.git", .branch("master")),
     ],
     targets: [
         .target(name: "FluentPostgresDriver", dependencies: ["FluentKit", "FluentSQL", "PostgresKit"]),
