1fdf8782958788fb85cb8e74272537ee0ff9a46d
Tanner <tannernelson@gmail.com>
2019-11-18 16:27:32 -0500

    beta 2: fluent db updates (#125)
    * beta 2: fluent db updates
    
    * psql id
    
    * add withConnection
    
    * withConnection updates
    
    * sqlkit updates
    
    * fix tests
    
    * regen linuxmain

--- Sources/FluentPostgresDriver/Exports.swift
+++ Sources/FluentPostgresDriver/Exports.swift
@@ -1,2 +1,8 @@
 @_exported import FluentKit
 @_exported import PostgresKit
+
+extension DatabaseID {
+    public static var psql: DatabaseID {
+        return .init(string: "psql")
+    }
+}
---
+++ Sources/FluentPostgresDriver/FluentPostgresDatabase.swift
@@ -0,0 +1,80 @@
+import FluentSQL
+
+struct _FluentPostgresDatabase {
+    let database: PostgresDatabase
+    let context: DatabaseContext
+}
+
+extension _FluentPostgresDatabase: Database {
+    func execute(query: DatabaseQuery, onRow: @escaping (DatabaseRow) -> ()) -> EventLoopFuture<Void> {
+        var expression = SQLQueryConverter(delegate: PostgresConverterDelegate())
+            .convert(query)
+        switch query.action {
+        case .create:
+            expression = PostgresReturning(expression)
+        default: break
+        }
+        let (sql, binds) = self.serialize(expression)
+        do {
+            return try self.query(sql, binds.map { try PostgresDataEncoder().encode($0) }) {
+                onRow($0)
+            }
+        } catch {
+            return self.eventLoop.makeFailedFuture(error)
+        }
+    }
+
+    func execute(schema: DatabaseSchema) -> EventLoopFuture<Void> {
+        let expression = SQLSchemaConverter(delegate: PostgresConverterDelegate())
+            .convert(schema)
+        let (sql, binds) = self.serialize(expression)
+        do {
+            return try self.query(sql, binds.map { try PostgresDataEncoder().encode($0) }) {
+                fatalError("unexpected row: \($0)")
+            }
+        } catch {
+            return self.eventLoop.makeFailedFuture(error)
+        }
+    }
+    
+    func withConnection<T>(_ closure: @escaping (Database) -> EventLoopFuture<T>) -> EventLoopFuture<T> {
+        self.database.withConnection {
+            closure(_FluentPostgresDatabase(database: $0, context: self.context))
+        }
+    }
+}
+
+extension _FluentPostgresDatabase: SQLDatabase {
+    var dialect: SQLDialect {
+        PostgresDialect()
+    }
+    
+    public func execute(
+        sql query: SQLExpression,
+        _ onRow: @escaping (SQLRow) -> ()
+    ) -> EventLoopFuture<Void> {
+        self.sql().execute(sql: query, onRow)
+    }
+}
+
+extension _FluentPostgresDatabase: PostgresDatabase {
+    func send(_ request: PostgresRequest, logger: Logger) -> EventLoopFuture<Void> {
+        self.database.send(request, logger: logger)
+    }
+    
+    func withConnection<T>(_ closure: @escaping (PostgresConnection) -> EventLoopFuture<T>) -> EventLoopFuture<T> {
+        self.database.withConnection(closure)
+    }
+}
+
+private struct PostgresReturning: SQLExpression {
+    let base: SQLExpression
+    init(_ base: SQLExpression) {
+        self.base = base
+    }
+
+    func serialize(to serializer: inout SQLSerializer) {
+        self.base.serialize(to: &serializer)
+        serializer.write(#" RETURNING id as "fluentID""#)
+    }
+}
