5c750a23e3f0fbba2c4679ac8d26d603fa5a268d
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-11-21 17:15:37 -0600

    Changed dependencies to Skelpo forks

--- Package.swift
+++ Package.swift
@@ -7,8 +7,8 @@ let package = Package(
         .library(name: "FluentPostgresDriver", targets: ["FluentPostgresDriver"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/fluent-kit.git", .branch("master")),
-        .package(url: "https://github.com/vapor/postgres-kit.git", .branch("master")),
+        .package(url: "https://github.com/skelpo/fluent-kit.git", from: "1.0.0-beta.2.2"),
+        .package(url: "https://github.com/skelpo/postgres-kit.git", from: "2.0.0-beta.1.1"),
     ],
     targets: [
         .target(name: "FluentPostgresDriver", dependencies: [
