c1c1c4a460cf4b6306680dea0831574b723c418a
tanner0101 <me@tanner.xyz>
2019-12-09 13:00:20 -0500

    fix converter delegate

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "fluent-postgres-driver",
+    platforms: [
+       .macOS(.v10_14)
+    ],
     products: [
         .library(name: "FluentPostgresDriver", targets: ["FluentPostgresDriver"]),
     ],
