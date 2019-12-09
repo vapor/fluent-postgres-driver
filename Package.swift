62f86950ee89f0a18281b9da9c460f8b8145782b
tanner0101 <me@tanner.xyz>
2019-12-09 14:18:30 -0500

    beta.2

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
         .library(name: "FluentPostgresDriver", targets: ["FluentPostgresDriver"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/fluent-kit.git", .branch("master")),
-        .package(url: "https://github.com/vapor/postgres-kit.git", .branch("master")),
+        .package(url: "https://github.com/vapor/fluent-kit.git", from: "1.0.0-beta.2"),
+        .package(url: "https://github.com/vapor/postgres-kit.git", from: "2.0.0-beta.2"),
     ],
     targets: [
         .target(name: "FluentPostgresDriver", dependencies: [
