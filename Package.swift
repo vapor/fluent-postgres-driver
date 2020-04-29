a5c044272414ecf41e31e4111215f744efd8fd7a
tanner0101 <me@tanner.xyz>
2020-04-29 09:55:01 -0400

    use release postgreskit

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/fluent-kit.git", from: "1.0.0-rc.1"),
-        .package(url: "https://github.com/vapor/postgres-kit.git", from: "2.0.0-rc.1"),
+        .package(url: "https://github.com/vapor/postgres-kit.git", from: "2.0.0"),
     ],
     targets: [
         .target(name: "FluentPostgresDriver", dependencies: [
