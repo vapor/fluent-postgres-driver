5956bc15e4a064bacbba39597cdb691414f28302
tanner0101 <me@tanner.xyz>
2020-06-12 17:51:55 -0400

    support dict data type

--- Sources/FluentPostgresDriver/PostgresConverterDelegate.swift
+++ Sources/FluentPostgresDriver/PostgresConverterDelegate.swift
@@ -3,27 +3,46 @@ import FluentSQL
 struct PostgresConverterDelegate: SQLConverterDelegate {
     func customDataType(_ dataType: DatabaseSchema.DataType) -> SQLExpression? {
         switch dataType {
-        case .uint8:
-            return SQLRaw(#""char""#)
         case .uuid:
             return SQLRaw("UUID")
         case .bool:
             return SQLRaw("BOOL")
         case .data:
             return SQLRaw("BYTEA")
+        case .date:
+            return SQLRaw("DATE")
         case .datetime:
             return SQLRaw("TIMESTAMPTZ")
         case .double:
             return SQLRaw("DOUBLE PRECISION")
-        case .json:
+        case .dictionary:
             return SQLRaw("JSONB")
+        case .array(of: let type):
+            if let type = type, let dataType = self.customDataType(type) {
+                return SQLArrayDataType(dataType: dataType)
+            } else {
+                return SQLRaw("JSONB")
+            }
         case .enum(let value):
             return SQLIdentifier(value.name)
-        default:
+        case .int8, .uint8:
+            return SQLRaw(#""char""#)
+        case .int16, .uint16:
+            return SQLRaw("SMALLINT")
+        case .int32, .uint32:
+            return SQLRaw("INT")
+        case .int64, .uint64:
+            return SQLRaw("BIGINT")
+        case .string:
+            return SQLRaw("TEXT")
+        case .time:
+            return SQLRaw("TIME")
+        case .float:
+            return SQLRaw("FLOAT")
+        case .custom:
             return nil
         }
     }
-
 
     func nestedFieldExpression(_ column: String, _ path: [String]) -> SQLExpression {
         switch path.count {
@@ -35,5 +54,13 @@ struct PostgresConverterDelegate: SQLConverterDelegate {
         default:
             fatalError()
         }
+    }
+}
+
+private struct SQLArrayDataType: SQLExpression {
+    let dataType: SQLExpression
+    func serialize(to serializer: inout SQLSerializer) {
+        self.dataType.serialize(to: &serializer)
+        serializer.write("[]")
     }
 }
