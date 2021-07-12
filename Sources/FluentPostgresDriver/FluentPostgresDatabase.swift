afe159ce915e752ab5f5b76479dc78a17051ce73
Tim Condon <0xTim@users.noreply.github.com>
2021-07-12 14:47:31 +0100

    Log SQL of queries in when log level set to debug mode (#180)
    * Log the SQL for any execute statements
    
    * Enable logging for create and delete enums as well

--- Sources/FluentPostgresDriver/FluentPostgresDatabase.swift
+++ Sources/FluentPostgresDriver/FluentPostgresDatabase.swift
@@ -24,6 +24,7 @@ extension _FluentPostgresDatabase: Database {
         default: break
         }
         let (sql, binds) = self.serialize(expression)
+        self.logger.debug("\(sql) \(binds)")
         do {
             return try self.query(sql, binds.map { try self.encoder.encode($0) }) {
                 onOutput($0.databaseOutput(using: self.decoder))
@@ -37,6 +38,7 @@ extension _FluentPostgresDatabase: Database {
         let expression = SQLSchemaConverter(delegate: PostgresConverterDelegate())
             .convert(schema)
         let (sql, binds) = self.serialize(expression)
+        self.logger.debug("\(sql) \(binds)")
         do {
             return try self.query(sql, binds.map { try self.encoder.encode($0) }) {
                 fatalError("unexpected row: \($0)")
@@ -53,6 +55,7 @@ extension _FluentPostgresDatabase: Database {
             for c in e.createCases {
                 _ = builder.value(c)
             }
+            self.logger.debug("\(builder.query)")
             return builder.run()
         case .update:
             if !e.deleteCases.isEmpty {
@@ -65,9 +68,12 @@ extension _FluentPostgresDatabase: Database {
             for create in e.createCases {
                 _ = builder.add(value: create)
             }
+            self.logger.debug("\(builder.query)")
             return builder.run()
         case .delete:
-            return self.sql().drop(enum: e.name).run()
+            let builder = self.sql().drop(enum: e.name)
+            self.logger.debug("\(builder.query)")
+            return builder.run()
         }
     }
 
@@ -76,7 +82,8 @@ extension _FluentPostgresDatabase: Database {
             return closure(self)
         }
         return self.database.withConnection { conn in
-            conn.simpleQuery("BEGIN").flatMap { _ in
+            self.logger.debug("BEGIN")
+            return conn.simpleQuery("BEGIN").flatMap { _ in
                 let db = _FluentPostgresDatabase(
                     database: conn,
                     context: self.context,
@@ -85,11 +92,13 @@ extension _FluentPostgresDatabase: Database {
                     inTransaction: true
                 )
                 return closure(db).flatMap { result in
-                    conn.simpleQuery("COMMIT").map { _ in
+                    self.logger.debug("COMMIT")
+                    return conn.simpleQuery("COMMIT").map { _ in
                         result
                     }
                 }.flatMapError { error in
-                    conn.simpleQuery("ROLLBACK").flatMapThrowing { _ in
+                    self.logger.debug("ROLLBACK")
+                    return conn.simpleQuery("ROLLBACK").flatMapThrowing { _ in
                         throw error
                     }
                 }
