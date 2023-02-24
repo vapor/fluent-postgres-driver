21b07885bc1b3c129068fa596c07409e9fb299f3
Tim Condon <0xTim@users.noreply.github.com>
2023-02-24 19:50:00 +0000

    Add API Docs (#202)

--- Sources/FluentPostgresDriver/Exports.swift
+++ Sources/FluentPostgresDriver/Exports.swift
@@ -1,5 +1,14 @@
+#if !BUILDING_DOCC
+
 @_exported import FluentKit
 @_exported import PostgresKit
+
+#else 
+
+import FluentKit
+import PostgresKit
+
+#endif
 
 extension DatabaseID {
     public static var psql: DatabaseID {
