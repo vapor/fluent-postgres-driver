fd2566ee6a5f7dd97253559c216b10c46517557a
Gwynne Raskind <gwynne@vapor.codes>
2023-03-03 09:01:40 -0600

    Fix hanging tests behavior caused by misuse of Fluent.

--- Tests/FluentPostgresDriverTests/FluentPostgresTransactionControlTests.swift
+++ Tests/FluentPostgresDriverTests/FluentPostgresTransactionControlTests.swift
@@ -7,58 +7,38 @@ import PostgresKit
 
 final class FluentPostgresTransactionControlTests: XCTestCase {
     
-    func testTransactionControl() throws {
-        try (self.db as! TransactionControlDatabase).beginTransaction().wait()
-        
-        let todo1 = Todo(title: "Test")
-        let todo2 = Todo(title: "Test2")
-        try todo1.save(on: self.db).wait()
-        try todo2.save(on: self.db).wait()
-        
-        try (self.db as! TransactionControlDatabase).commitTransaction().wait()
-        
-        let count = try Todo.query(on: self.db).count().wait()
-        XCTAssertEqual(count, 2)
-    }
-    
     func testRollback() throws {
-        try (self.db as! TransactionControlDatabase).beginTransaction().wait()
-        
-        let todo1 = Todo(title: "Test")
-        
-        try todo1.save(on: self.db).wait()
-        
-        let duplicate = Todo(title: "Test")
-        var errorCaught = false
-        
         do {
-            try duplicate.create(on: self.db).wait()
+            try self.db.withConnection { db in
+                (db as! TransactionControlDatabase).beginTransaction().flatMap {
+                    let todo1 = Todo(title: "Test")
+                    return todo1.save(on: db)
+                }.flatMap {
+                    let duplicate = Todo(title: "Test")
+                    return duplicate.create(on: db)
+                        .flatMap {
+                            (db as! TransactionControlDatabase).commitTransaction()
+                        }.flatMapError { e in
+                            return (db as! TransactionControlDatabase).rollbackTransaction()
+                                .flatMap { db.eventLoop.makeFailedFuture(e) }
+                        }
+                }
+            }.wait()
+            XCTFail("Expected error but none was thrown")
+        } catch PostgresError.server(let e) where e.fields[.sqlState] == "23505" {
+            // ignore
         } catch {
-            errorCaught = true
-            try (self.db as! TransactionControlDatabase).rollbackTransaction().wait()
+            XCTFail("Expected SQL state 23505 but got \(error)")
         }
-        
-        if !errorCaught {
-            try (self.db as! TransactionControlDatabase).commitTransaction().wait()
-        }
-        
-        XCTAssertTrue(errorCaught)
+
         let count2 = try Todo.query(on: self.db).count().wait()
         XCTAssertEqual(count2, 0)
     }
     
-    var benchmarker: FluentBenchmarker {
-        return .init(databases: self.dbs)
-    }
     var eventLoopGroup: EventLoopGroup!
     var threadPool: NIOThreadPool!
     var dbs: Databases!
-    var db: Database {
-        self.benchmarker.database
-    }
-    var postgres: PostgresDatabase {
-        self.db as! PostgresDatabase
-    }
+    var db: Database!
     
     override func setUpWithError() throws {
         try super.setUpWithError()
@@ -69,15 +49,10 @@ final class FluentPostgresTransactionControlTests: XCTestCase {
         self.dbs = Databases(threadPool: threadPool, on: self.eventLoopGroup)
 
         self.dbs.use(.testPostgres(subconfig: "A"), as: .a)
-        self.dbs.use(.testPostgres(subconfig: "B"), as: .b)
 
-        let a = self.dbs.database(.a, logger: Logger(label: "test.fluent.a"), on: self.eventLoopGroup.next())
-        _ = try (a as! PostgresDatabase).query("drop schema public cascade").wait()
-        _ = try (a as! PostgresDatabase).query("create schema public").wait()
-
-        let b = self.dbs.database(.b, logger: Logger(label: "test.fluent.b"), on: self.eventLoopGroup.next())
-        _ = try (b as! PostgresDatabase).query("drop schema public cascade").wait()
-        _ = try (b as! PostgresDatabase).query("create schema public").wait()
+        self.db = self.dbs.database(.a, logger: Logger(label: "test.fluent.a"), on: self.eventLoopGroup.any())
+        _ = try (self.db as! PostgresDatabase).query("drop schema public cascade").wait()
+        _ = try (self.db as! PostgresDatabase).query("create schema public").wait()
         
         try CreateTodo().prepare(on: self.db).wait()
     }
