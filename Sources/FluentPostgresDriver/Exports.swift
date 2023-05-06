2319be2a8e4abfbe57f5cc8f9f249a1c36ac611c
Gwynne Raskind <gwynne@vapor.codes>
2023-05-05 22:22:08 -0500

    Require Swift 5.7

--- Sources/FluentPostgresDriver/Exports.swift
+++ Sources/FluentPostgresDriver/Exports.swift
@@ -1,12 +1,12 @@
-#if !BUILDING_DOCC
+#if swift(>=5.8)
+
+@_documentation(visibility: internal) @_exported import FluentKit
+@_documentation(visibility: internal) @_exported import PostgresKit
+
+#else
 
 @_exported import FluentKit
 @_exported import PostgresKit
-
-#else 
-
-import FluentKit
-import PostgresKit
 
 #endif
 
