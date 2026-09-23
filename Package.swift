cf36df37b902577449cbb3c16de6901b05ff5660
Paul Toffoloni <paultoffoloni@gmail.com>
2026-09-23 09:09:32 +0200

    Add doc to new config factory

--- Package.swift
+++ Package.swift
@@ -17,7 +17,8 @@ let package = Package(
         .package(url: "https://github.com/vapor/postgres-kit.git", from: "2.14.1"),
         .package(url: "https://github.com/vapor/async-kit.git", from: "1.21.0"),
         .package(url: "https://github.com/vapor/postgres-nio.git", from: "1.33.1"),
-        .package(url: "https://github.com/swift-server/swift-service-lifecycle.git", from: "2.12.0")
+        .package(url: "https://github.com/swift-server/swift-service-lifecycle.git", from: "2.12.0"),
+        .package(url: "https://github/apple/swift-log.git", from: "1.14.0")
     ],
     targets: [
         .target(
@@ -28,7 +29,8 @@ let package = Package(
                 .product(name: "PostgresKit", package: "postgres-kit"),
                 .product(name: "AsyncKit", package: "async-kit"),
                 .product(name: "PostgresNIO", package: "postgres-nio"),
-                .product(name: "ServiceLifecycle", package: "swift-service-lifecycle")
+                .product(name: "ServiceLifecycle", package: "swift-service-lifecycle"),
+                .product(name: "Logging", package: "swift-log")
             ],
             swiftSettings: swiftSettings
         ),
